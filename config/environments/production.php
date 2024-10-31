3d4a43a959cec1ba80019a0c45056c1dcc2ecce5
cbally <cyrille_45@hotmail.com>
2024-10-31 23:26:16 +0100

    Update production.php

--- config/environments/production.php
+++ config/environments/production.php
@@ -1,19 +1,1 @@
-<?php
-/**
- * Configuration overrides for WP_ENV === 'staging'
- */
 
-use Roots\WPConfig\Config;
-
-/**
- * You should try to keep staging as close to production as possible. However,
- * should you need to, you can always override production configuration values
- * with `Config::define`.
- *
- * Example: `Config::define('WP_DEBUG', true);`
- * Example: `Config::define('DISALLOW_FILE_MODS', false);`
- */
-Config::define('WP_ENV', 'production');
-Config::define('WP_HOME', 'https://mytibo.io');
-Config::define('WP_SITEURL', 'https://mytibo.io/wp');
-Config::define('CC_PHP_VERSION', 8);
